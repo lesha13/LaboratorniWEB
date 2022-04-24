088719faf83e7b07fca6767ced5ca3bfc176bdff
lesha13 <leshamashuna@gmail.com>
2022-04-24 09:51:51 +0300

    added lab 4.3

---
+++ lab 4/lab 4.3/5.js
@@ -0,0 +1,24 @@
+// 5
+let mood = [
+    { 
+        mood:"Веселий",
+        url:"https://w7.pngwing.com/pngs/180/962/png-transparent-smiley-emoticon-smiley-miscellaneous-smiley-desktop-wallpaper-thumbnail.png "
+    },
+    {
+       mood:"Сумний",
+       url:"https://w7.pngwing.com/pngs/222/140/png-transparent-sad-emoticon-iphone-emoji-sadness-smiley-emoticon-emoji-face-electronics-face-emoji-face-thumbnail.png "
+    },
+    {
+        mood:"Цукерберг",
+        url:"https://itc.ua/wp-content/uploads/2019/10/190305_fiat_concept_centoventi_12-39.jpg "
+    }
+]
+
+document.getElementById("show").onclick = () =>
+{
+    let numberMood = document.getElementById("mood").value
+    document.getElementById("img").innerHTML = `
+    <p>${mood[numberMood-1].mood}</p>
+    <img src="${mood[numberMood-1].url}">`
+}
+
---
+++ lab 4/lab 4.3/Arrays.js
@@ -0,0 +1,84 @@
+function Random(min, max) 
+{
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min
+}
+
+// 1
+let K = parseInt(prompt("Input number of subjects", "5"))
+let marks = Array()
+
+for (let i = 1; i <= K; i++) {
+    marks.push(parseInt(prompt("Mark: ", "")))
+}
+marks = marks.sort((a, b) => a - b)
+alert(marks)
+
+let average = marks.reduce((previousValue, currentValue) => previousValue + currentValue, 0) / K
+
+alert(`Average ${average}`)
+
+switch(marks[0]){
+    case 1:
+        alert(`Двійочник`)
+        break;
+    case 2:
+        alert(`Двійочник`)
+        break; 
+    case 3:
+        alert(`Трійочник`)
+        break; 
+    case 4:
+        alert(`Хорошист`)
+        break; 
+    case 5:
+        alert(`Відмінник`)
+        break; 
+}
+
+// 2
+let days = Array()
+for (let i = 1; i <= 7; i++) {
+    days.push(parseInt(prompt("Number: ", "")))
+}
+
+for (let i = 0; i <= 6; i++) {
+    if (days[i]) {alert(i+1)}}
+alert(`min: ${Math.min(days)}`)
+alert(`max: ${Math.max(days)}`)
+alert(`all: ${days.reduce((previousValue, currentValue) => previousValue + currentValue, 0)}`)
+alert(`saturday: ${days[5]}, sunday: ${days[6]}`)
+
+// 3
+let name = prompt("Ім'я: ", "Юрій")
+let names = ["Юрій", "Юрій", "Юрій", "Юрій", "aaa", "bbb", "ccc"]
+
+alert(names.filter(el => el === name).length)
+
+// 4
+let prises = Array()
+let used = Array()
+for (let i = 1; i <= 10; i++) {
+    prises.push(Random(-500, 500))
+}
+alert(prises)
+
+
+let prise = 0
+for (let i = 1; i <= 10; i++) {
+    let answear = prompt("type number or 'stop' ", "")
+    if (answear == 'stop')
+    {
+        break
+    }
+    else
+    {
+        used.push(answear)
+        prise += prises[parseInt(answear)-1]
+        alert(prise)
+        alert(used)
+    }
+}
+
+alert(`Result: ${prise}`)
